e684526233b2839bd60e10c54fe39be6c1b5b465
namwob44 <michael.bowman44@gmail.com>
2023-10-30 09:26:10 -0400

    Update annotate_variants.R
    extra help function information

--- R/annotate_variants.R
+++ R/annotate_variants.R
@@ -1,5 +1,9 @@
 #' Annotate variants of interest
-#'
+#' This function takes in h5 files to extract DNA information through given TXDB files (primarily hg19)
+#' The gene names are mapped for their specific nucleotide positions. The starting and ending positions
+#' for the chromosome are listed. The consequence of the mutation such as synonymous, nonsynonymous,
+#' as well as if this is a coding region, on the exon boundry or intronic. Short amino acid changes are also labeled.
+#' The variance matrix also extracts the amplicon that the variant is found on.
 #' @param file path to h5 file to pull out relevant DNA information
 #' @param select_variants variants of interest, default is all variants
 #' @importFrom BSgenome.Hsapiens.UCSC.hg19 Hsapiens
