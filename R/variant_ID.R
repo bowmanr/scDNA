55b2447de5909a0583e9e4e3c0e3a2aa7135338c
namwob44 <michael.bowman44@gmail.com>
2023-10-30 10:34:34 -0400

    Update variant_ID.R
    additional content in help information

--- R/variant_ID.R
+++ R/variant_ID.R
@@ -1,11 +1,11 @@
 #' Variant identification and frequency tallies
-#'
+#' The DNA variants for each cell are pulled from the specified H5 files. Each variant for each cell is genotyped to be WildType, Heterozygous, Homozygous or Missing. The genotyping rate is determined by taking WT+Het+Hom over total cell calls (including missing). The VAF is determined by number of allele copies we see in a weighted sum. A filter is applied to both of these calculatins to include or exclude variants of interest. These are then annotated to include the variant information such as gene name, nucleotide location, and short amino acid changes.
 #' @param file path to the h5 file
 #' @param panel name of prebuilt panel/txdb
 #' @param GT_cutoff Fraction of cells that are successfully genotyped for initial filtering (default 0.2, meaning 20%)
 #' @param VAF_cutoff Fraction of cells that are mutated for initial filtering of variants (default 0.005, meaning 0.05%)
 #'
-#' @return A dataframe with each variant on a row, and tally of the number of cells that are WT, Het, Hom or missing for a mutation. Calculated VAF and gentoyping freuqency is also provided. If multiple samples are present in the h5 file, a list object will be returned with each sample as an entry in the list
+#' @return A dataframe with each variant on a row, and tally of the number of cells that are WT, Het, Hom or missing for a mutation. Calculated VAF and gentoyping frequency is also provided. If multiple samples are present in the h5 file, a list object will be returned with each sample as an entry in the list
 #' @export
 #'
 #' @examples
